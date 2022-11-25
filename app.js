36a579bcda0aba89f02e0d02c413bcd312b8e66e
Jose Ramirez <26813102+RamirezJA@users.noreply.github.com>
2022-11-25 00:32:35 -0800

    play/pause+loop

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ class DrumKit {
     this.hihatAudio = document.querySelector(".hihat-sound")
     this.index = 0
     this.bpm = 150
+    this.isPlaying = null
   }
   activePad() {
     this.classList.toggle("active")
@@ -14,15 +15,49 @@ class DrumKit {
   repeat() {
     let step = this.index % 8
     const activeBars = document.querySelectorAll(`.b${step}`)
-
+    //Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`
+      //check if pads are active
+      if (bar.classList.contains("active")) {
+        //check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0
+          this.kickAudio.play()
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0
+          this.snareAudio.play()
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0
+          this.hihatAudio.play()
+        }
+      }
+    })
     this.index++
   }
   start() {
-    console.log(this)
     const interval = (60 / this.bpm) * 1000
-    setInterval(() => {
-      this.repeat()
-    }, interval)
+    //check if its playing
+    if (!this.isPlaying) {
+      this.isPlaying = setInterval(() => {
+        this.repeat()
+      }, interval)
+    } else {
+      //clear the interval
+      clearInterval(this.isPlaying)
+      this.isPlaying = null
+    }
+  }
+  updateBtn() {
+    if (!this.isPlaying) {
+      this.playBtn.innerText = "Stop"
+      this.playBtn.classList.add("active")
+    } else {
+      this.playBtn.innerText = "Play"
+      this.playBtn.classList.remove("active")
+    }
   }
 }
 
@@ -30,8 +65,12 @@ const drumKit = new DrumKit()
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad)
+  pad.addEventListener("animationend", function () {
+    this.style.animation = ""
+  })
 })
 
 drumKit.playBtn.addEventListener("click", function () {
+  drumKit.updateBtn()
   drumKit.start()
 })
